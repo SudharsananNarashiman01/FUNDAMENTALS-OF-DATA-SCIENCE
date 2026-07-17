0da8f5f30247400670d7b3c044a9bcf936f519e3
SudharsananNarashiman01 <narashimansudhan@gmail.com>
2026-07-17 14:14:08 +0530

    Add files via upload

---
+++ LAB PROGRAM/p2.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+# Load CSV
+df = pd.read_csv(r"C:\Users\tthul\Downloads\email_spam(1).csv")
+
+print("Dataset:")
+print(df)
+
+# Features and Target
+X = df[['Num_Links', 'Num_Caps_Words', 'Email_Length']]
+y = df['Spam']
+
+# Train Model
+model = LogisticRegression()
+model.fit(X, y)
+
+# User Input
+links = int(input("Enter Number of Links: "))
+caps = int(input("Enter Number of Capitalized Words: "))
+length = int(input("Enter Email Length: "))
+
+prediction = model.predict([[links, caps, length]])
+
+if prediction[0] == 1:
+    print("Prediction: Spam Email")
+else:
+    print("Prediction: Not Spam")
